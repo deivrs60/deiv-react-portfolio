a65d0b6fbaf1da9395dd3ca0f39b022ddb73601f
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-28 23:40:53 -0700

    initial build. about me and nav component bases

---
+++ photo-port/src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import _image_ from "../../assets/"
+
+function About() {
+
+    return (
+        <section className="my-5">
+            <h1 id="about">Who am I?</h1>
+            {/* img src={_img_} className="" style={{ width }} alt=" " */}
+        </section>
+        
+    );
+}
+
+
+export default About;
