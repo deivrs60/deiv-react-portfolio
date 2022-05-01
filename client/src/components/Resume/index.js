71a818d7793d24eecc66430b24fe18f0ae607051
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-05-01 02:11:19 -0700

    can now download resume from the resume component

--- client/src/components/Resume/index.js
+++ client/src/components/Resume/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import resume from '../../assets/files/DeivResume.pdf'
+
 
 
 function Resume() {
 
     return (
-        <section className="my-5">
-            This is my resume 
-        </section>
+       <div className="resume-body">
+           <h2 className="p-4"> Resume </h2>
+           <div className="download-here">
+               <h3 className="p-3">
+                   Please feel free to download my resume
+                   <a href={resume} target="_blank" rel="noopener noreferrer">HERE</a>
+               </h3>
+           </div>
+        </div>
         
     );
 }
