ea640024dd79691efb60952df318b4cf360bf184
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-05-01 04:14:57 -0700

    added another .gitignore, noticed my .DS_Store was viewable

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
+
+    const [
+        formState,
+        setFormState
+    ] = useState({ name: '', email: '', message: '',});
+    
+    const { name, email, message } = formState;
+    const [
+        errorMessage,
+        setErrorMessage
+    ] = useState('');
+
+    function handleChange(e) {
+        if(e.target.name === 'email') {
+            const isValidEmail = validateEmail(e.target.value);
+            if(!isValidEmail) {
+                setErrorMessage('Please enter a valid email!');
+            } else {
+                setErrorMessage('');
+            }
+        } else 
+    }
+
+
+
 
     return (
         <section className="my-5">
