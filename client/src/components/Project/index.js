607fd95f6d9e0f9cf9f3c597f2ab551c5fcca91d
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-05-01 14:34:34 -0700

    added basic portfolio functionality

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import {FaGithub} from "react-icons/fa";
+import sunnyweekend from '../../assets/images/sunnyweekend.png';
 
 
-function ProjectPortfolio() {
+export default function ProjectPortfolio() {
 
     return (
-        <section className="my-5">
-            This is the Project Page
-        </section>
-        
-    );
+    <section className="row row-cols-1 row-cols-md-2 g-4 py-5 mt-5">
+        <div className="container-fluid">
+
+            <div className ="row">
+                <div class="col-lgg-6- cold-md-6 cold-sm-6 col-xs-12">
+                    <div class="hovereffect">
+                        <a href="https://deivrs60.github.io/sunny-weekend/" target="_bank" rel="noreferrer">
+                            <img src ={sunnyweekend} 
+                                alt="Sunny Weekend"
+                                className="img-fluid"
+                            />
+                            <div class="overlay">
+                                <h2> Sunny Weekend
+                                    <a href="https://github.com/deivrs60/sunny-weekend">
+                                        <FaGithub />
+                                    </a> 
+                                </h2>
+                                <h3> HTML, CSS, APIs </h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+     );
 }
 
-
-export default ProjectPortfolio;
