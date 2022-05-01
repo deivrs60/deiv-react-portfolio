ac10eabd5386c50bb8f3bb40223aa5ea6e9e0353
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-05-01 04:38:58 -0700

    added form input to contact component

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -22,16 +22,46 @@ function Contact() {
             } else {
                 setErrorMessage('');
             }
-        } else 
-    }
+        } else {
+            if(!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required!`)
+            } else { 
+                setErrorMessage('');
+            }
+        } if (!errorMessage) {
+            setFormState({ 
+                ...formState, 
+                [e.target.name]: e.target.value 
+            });
+        }
+    };
 
 
 
 
     return (
-        <section className="my-5">
-            This is the Contact Page
-        </section>
+        <form id="contact-form" onSubmit= { handleSubmit }>
+            <div className="form-container">
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label px-2">Name</label>
+                    <input type="text" className="form-control" name="name" defaultValue={name} onBlue ={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label px-2">Email</label>
+                    <input type="email" className="form-control" name="email" defaultValue={email} onBlue ={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="message" className="form-label px-2">Message</label>
+                    <textarea className="form-control" name="message" defaultValue={message} onBlue ={handleChange} rows="5"></textarea>
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button className="btn btn-primary" type="submit">Submit</button>
+            </div>
+        </form>
         
     );
 }
