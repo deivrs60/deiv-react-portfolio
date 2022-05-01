56af194368505fb16bfb68765ab2641573e23eac
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-05-01 02:39:15 -0700

    Resume page content rendered

--- client/src/components/Resume/index.js
+++ client/src/components/Resume/index.js
@@ -7,14 +7,48 @@ function Resume() {
 
     return (
        <div className="resume-body">
-           <h2 className="p-4"> Resume </h2>
+           <h2 className="p-4">Resume</h2>
            <div className="download-here">
                <h3 className="p-3">
                    Please feel free to download my resume
-                   <a href={resume} target="_blank" rel="noopener noreferrer">HERE</a>
+                   <a href={resume} target="_blank" rel="noopener noreferrer"> HERE </a>
                </h3>
            </div>
+           <div className="resumebody p-3">
+               <div className="fe-experience">
+                   <h3>Front-End Experience</h3>
+                   <ul>
+                       <li>HTML</li>
+                       <li>CSS</li>
+                       <li>Responsive Design</li>
+                       <li>JavaScript</li>
+                       <li>jQuery</li>
+                       <li>Bootstrap</li>
+                       <li>React</li>
+                   </ul>
+               </div>
+               <div className="be-experience">
+                   <h3>Back-End Experience</h3>
+                   <ul>
+                       <li>APIs</li>
+                       <li>Node.JS</li>
+                       <li>Express.JS</li>
+                       <li>MySQL</li>
+                       <li>Sequelize</li>
+                       <li>Heroku</li>
+                       <li>NoSQL</li>
+                       <li>Mongoose</li>
+                       <li>MongoDB</li>
+                       <li>MongoDB Atlas</li>
+                       <li>Restful APIs</li>
+                       <li>Apollo</li>
+                       <li>GraphQL</li>
+                       <li>Insomnia</li>
+                   </ul>
+               </div>
+           </div>
         </div>
+        
         
     );
 }
